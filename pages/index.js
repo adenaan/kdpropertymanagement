84578e0c995645f0940d0517bc9ce0c6492668c9
Adenaan <adenaan@gmail.com>
2025-05-26 21:38:21 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,46 +1,48 @@
-import fs from 'fs';
-import path from 'path';
-import Link from 'next/link';
-
-export async function getStaticProps() {
-  const propertiesDir = path.join(process.cwd(), 'content/properties');
-  const filenames = fs.readdirSync(propertiesDir);
-
-  const properties = filenames.map(filename => {
-    const filePath = path.join(propertiesDir, filename);
-    const fileContents = fs.readFileSync(filePath, 'utf8');
-    return JSON.parse(fileContents);
-  });
-
-  return {
-    props: { properties },
-  };
-}
+import fs from 'fs'
+import path from 'path'
+import Link from 'next/link'
+import { slugify } from '../lib/slugify'
 
 export default function Home({ properties }) {
   return (
     <div>
       <h1>Properties</h1>
       <ul>
-        {properties.map((property) => (
-          <li key={property.slug}>
-            <Link href={`/property/${property.slug}`}>
-              <a>
-                <h2>{property.title}</h2>
-                <p>{property.location}</p>
-                <p>Price per night: ${property.price}</p>
-                {property.images && property.images.length > 0 && (
-                  <img 
-                    src={property.images[0].image} 
-                    alt={property.title} 
-                    style={{ width: '200px', height: 'auto' }} 
-                  />
-                )}
-              </a>
-            </Link>
-          </li>
-        ))}
+        {properties.map((property) => {
+          const slug = property.slug ? slugify(property.slug) : slugify(property.title);
+          return (
+            <li key={slug}>
+              <Link href={`/property/${slug}`}>
+                <a>{property.title}</a>
+              </Link>
+            </li>
+          )
+        })}
       </ul>
     </div>
-  );
+  )
+}
+
+export async function getStaticProps() {
+  const propertiesDir = path.join(process.cwd(), 'content', 'properties')
+  const filenames = fs.readdirSync(propertiesDir)
+
+  const properties = filenames.map((filename) => {
+    const filePath = path.join(propertiesDir, filename)
+    const fileContents = fs.readFileSync(filePath, 'utf8')
+    const data = JSON.parse(fileContents)
+
+    // Add sanitized slug here to pass to props (optional)
+    const slug = data.slug ? slugify(data.slug) : slugify(data.title)
+    return {
+      ...data,
+      slug,
+    }
+  })
+
+  return {
+    props: {
+      properties,
+    },
+  }
 }
