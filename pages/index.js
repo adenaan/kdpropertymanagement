24f53fe6c720d756365946ac74f1690b160de552
Adenaan <adenaan@gmail.com>
2025-05-26 22:15:11 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,66 @@
 import fs from 'fs'
 import path from 'path'
 import Link from 'next/link'
-import { slugify } from '../lib/slugify'
+import { slugify } from '../lib/slugify'import fs from 'fs'
+import path from 'path'
+
+export default function Property({ property }) {
+  return (
+    <div>
+      <h1>{property.title}</h1>
+      <p>{property.description}</p>
+      {/* render other property details */}
+    </div>
+  )
+}
+
+export async function getStaticPaths() {
+  const propertiesDir = path.join(process.cwd(), 'content', 'properties')
+  const filenames = fs.readdirSync(propertiesDir)
+
+  const paths = filenames.map(filename => {
+    const filePath = path.join(propertiesDir, filename)
+    const fileContents = fs.readFileSync(filePath, 'utf8')
+    const data = JSON.parse(fileContents)
+    const slug = data.slug ? slugify(data.slug) : slugify(data.title)
+
+    return { params: { slug } }
+  })
+
+  return {
+    paths,
+    fallback: false, // or true/blocking depending on your needs
+  }
+}
+
+export async function getStaticProps({ params }) {
+  const { slug } = params
+  const propertiesDir = path.join(process.cwd(), 'content', 'properties')
+  const filenames = fs.readdirSync(propertiesDir)
+
+  const propertyFile = filenames.find(filename => {
+    const filePath = path.join(propertiesDir, filename)
+    const fileContents = fs.readFileSync(filePath, 'utf8')
+    const data = JSON.parse(fileContents)
+    const fileSlug = data.slug ? slugify(data.slug) : slugify(data.title)
+    return fileSlug === slug
+  })
+
+  if (!propertyFile) {
+    return {
+      notFound: true,
+    }
+  }
+
+  const filePath = path.join(propertiesDir, propertyFile)
+  const fileContents = fs.readFileSync(filePath, 'utf8')
+  const property = JSON.parse(fileContents)
+
+  return {
+    props: { property },
+  }
+}
+
 
 export default function Home({ properties }) {
   return (
