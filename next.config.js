c08411d0b90d13fa2bf57f0af7c2f1b7e7e7383e
Adenaan <adenaan@gmail.com>
2025-05-26 15:48:43 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export', // 🔥 This replaces next export
+  output: 'export',
+  // other Next.js config options...
 };
 
-module.exports = nextConfig;
+export default nextConfig;
