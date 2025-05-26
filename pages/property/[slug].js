fad4050fa8f88c5f4b2bca26298f8148eb668c2a
Adenaan <adenaan@gmail.com>
2025-05-26 15:36:37 +0200

    Update [slug].js

--- pages/property/[slug].js
+++ pages/property/[slug].js
@@ -1,1 +1,31 @@
+import React from "react";
 
+export default function PropertyPage({ property }) {
+  return (
+    <div>
+      <h1>{property.title}</h1>
+      <p>{property.description}</p>
+      {/* other property details */}
+    </div>
+  );
+}
+
+export async function getStaticPaths() {
+  // Generate paths for each property
+  return {
+    paths: [],
+    fallback: false,
+  };
+}
+
+export async function getStaticProps({ params }) {
+  // Fetch property data
+  return {
+    props: {
+      property: {
+        title: "Example Property",
+        description: "A lovely place to stay.",
+      },
+    },
+  };
+}
