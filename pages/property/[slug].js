4d76d5d010cefb312460da18971c24183ef16bc3
Adenaan <adenaan@gmail.com>
2025-05-26 22:32:45 +0200

    Update [slug].js

--- pages/property/[slug].js
+++ pages/property/[slug].js
@@ -1,3 +1,4 @@
+// pages/property/[slug].js
 import fs from 'fs'
 import path from 'path'
 import { slugify } from '../../lib/slugify'
@@ -26,13 +27,19 @@ export async function getStaticProps({ params }) {
   const propertiesDir = path.join(process.cwd(), 'content', 'properties')
   const filenames = fs.readdirSync(propertiesDir)
 
-  const propertyFile = filenames.find(filename => {
+  let propertyFile = null
+
+  for (const filename of filenames) {
     const filePath = path.join(propertiesDir, filename)
     const fileContents = fs.readFileSync(filePath, 'utf8')
     const data = JSON.parse(fileContents)
     const fileSlug = data.slug ? slugify(data.slug) : slugify(data.title)
-    return fileSlug === slug
-  })
+
+    if (fileSlug === slug) {
+      propertyFile = filename
+      break
+    }
+  }
 
   if (!propertyFile) {
     return { notFound: true }
@@ -48,11 +55,12 @@ export async function getStaticProps({ params }) {
 }
 
 export default function Property({ property }) {
+  if (!property) return <div>Property not found.</div>
+
   return (
     <div>
       <h1>{property.title}</h1>
       <p>{property.description}</p>
-      {/* Render other property details */}
     </div>
   )
 }
